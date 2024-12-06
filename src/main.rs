d44c28f27001decf05ce2cf4c89deda0d082758b
galah92 <galaharoni92@gmail.com>
2024-12-06 08:46:58 +0200

    Added code to fetch RSS feeds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use feed_rs::parser;
+
+async fn fetch_last_articles(url: &str, count: usize) -> anyhow::Result<Vec<String>> {
+    let content = reqwest::get(url).await?.bytes().await?;
+    let feed = parser::parse(&content[..])?;
+
+    Ok(feed
+        .entries
+        .iter()
+        .take(count)
+        .map(|entry| {
+            format!(
+                "{}: {}",
+                entry
+                    .title
+                    .as_ref()
+                    .map(|t| t.content.as_str())
+                    .unwrap_or_default(),
+                entry
+                    .links
+                    .first()
+                    .map(|l| l.href.as_str())
+                    .unwrap_or_default()
+            )
+        })
+        .collect())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let articles = fetch_last_articles("https://simonwillison.net/atom/everything/", 3).await?;
+    for article in articles {
+        println!("{}", article);
+    }
+    Ok(())
 }
